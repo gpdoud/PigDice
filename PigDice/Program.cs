30b2308a769ebe072f298bbbed51d774c23a6f12
Greg doud <gpdoud@gmail.com>
2017-09-06 17:01:56 -0400

    Done

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace PigDice {
 	class Program {
+		Random rnd = new Random();
+
+		int RollDice() {
+			return rnd.Next(6) + 1;
+		}
+		int RunPigDice() {
+			var Total = 0;
+			int Die = 0;
+			do {
+				Die = RollDice();
+				if(Die != 1) {
+					Total += Die;
+				}
+			} while (Die != 1);
+			//Console.WriteLine($"Total this game is {Total}");
+			return Total;
+		}
+		void Run() {
+			var BestTotal = 0;
+			//for (var idx = 0; idx < 1000000; idx++) {
+			var counter = 0;
+			while(counter++ < 100) { 
+				var ThisGame = RunPigDice();
+				BestTotal = (ThisGame > BestTotal) ? ThisGame : BestTotal;
+			}
+			Console.WriteLine($"Best game score is {BestTotal} in only {counter-1} rolls.");
+		}
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
